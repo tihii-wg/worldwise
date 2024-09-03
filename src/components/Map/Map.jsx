504c9921e52a503929c9fc04a96e38efe4977808
Vova <podolian@gmail.com>
2024-09-03 22:20:23 +0300

    Geologation on map

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,5 +1,6 @@
 import { useNavigate, useParams, useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
+import { Marker, Popup, MapContainer, TileLayer } from "react-leaflet";
 
 function Map() {
   const { id } = useParams();
@@ -7,20 +8,26 @@ function Map() {
   const lat = serchParams.get("lat");
   const lng = serchParams.get("lng");
 
-  const navigate = useNavigate();
+  const position ={ [lat, lng]};
 
   return (
-    <div
-      className={styles.mapContainer}
-      onClick={() => {
-        navigate("form");
-      }}
-    >
-      <h1>map</h1>
-      <h1>id:{id}</h1>
-      <h1>
-        position:{lat},{lng}
-      </h1>
+    <div className={styles.mapContainer}>
+      <MapContainer
+        className={styles.map}
+        center={position}
+        zoom={13}
+        scrollWheelZoom={true}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={position}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
     </div>
   );
 }
