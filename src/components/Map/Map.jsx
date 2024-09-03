6788b3460b08891159e68f1b1c6ea5521a1eecda
Vova <podolian@gmail.com>
2024-09-03 22:21:14 +0300

    new

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,35 +1,56 @@
 import { useNavigate, useParams, useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
-import { Marker, Popup, MapContainer, TileLayer } from "react-leaflet";
+import { Marker, Popup, MapContainer, TileLayer, useMap } from "react-leaflet";
+import { useCities } from "../Contexts/CityContext";
+import { useEffect, useState } from "react";
 
 function Map() {
+  const { cities } = useCities();
   const { id } = useParams();
-  const [serchParams, setSerchParams] = useSearchParams();
+  const [serchParams] = useSearchParams();
   const lat = serchParams.get("lat");
   const lng = serchParams.get("lng");
 
-  const position ={ [lat, lng]};
-
+  const [position, setPosition] = useState([40, 0]);
+  console.log(position);
+  useEffect(
+    function () {
+      if (lat && lng) setPosition([lat, lng]);
+    },
+    [lat, lng]
+  );
   return (
     <div className={styles.mapContainer}>
       <MapContainer
         className={styles.map}
+        //  center={[51.505, -0.09]}
         center={position}
-        zoom={13}
+        zoom={6}
         scrollWheelZoom={true}
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
         />
-        <Marker position={position}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
+        {cities.map((city) => (
+          <Marker
+            position={[city.position.lat, city.position.lng]}
+            key={city.id}
+          >
+            <Popup>
+              <span>{city.emoji}</span>
+              <span>{city.cityName}</span>
+            </Popup>
+          </Marker>
+        ))}
+        <ChengeCenter position={position} />
       </MapContainer>
     </div>
   );
 }
-
+function ChengeCenter({ position }) {
+  const map = useMap();
+  map.setView(position, 10);
+  return null;
+}
 export default Map;
