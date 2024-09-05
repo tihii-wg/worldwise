e740cd3688020c4c75ddea922282ab34ef207e22
Vova <podolian@gmail.com>
2024-09-05 20:58:08 +0300

    SetForm

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,29 +1,36 @@
 import { useNavigate, useParams, useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
-import { Marker, Popup, MapContainer, TileLayer, useMap } from "react-leaflet";
+import {
+  Marker,
+  Popup,
+  MapContainer,
+  TileLayer,
+  useMap,
+  useMapEvents,
+} from "react-leaflet";
 import { useCities } from "../Contexts/CityContext";
 import { useEffect, useState } from "react";
 
 function Map() {
   const { cities } = useCities();
   const { id } = useParams();
+  const [position, setPosition] = useState([40, 0]);
+
   const [serchParams] = useSearchParams();
   const lat = serchParams.get("lat");
   const lng = serchParams.get("lng");
 
-  const [position, setPosition] = useState([40, 0]);
-  console.log(position);
   useEffect(
     function () {
       if (lat && lng) setPosition([lat, lng]);
     },
     [lat, lng]
   );
+
   return (
     <div className={styles.mapContainer}>
       <MapContainer
         className={styles.map}
-        //  center={[51.505, -0.09]}
         center={position}
         zoom={6}
         scrollWheelZoom={true}
@@ -33,16 +40,14 @@ function Map() {
           url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
         />
         {cities.map((city) => (
-          <Marker
-            position={[city.position.lat, city.position.lng]}
-            key={city.id}
-          >
+          <Marker position={position} key={city.id}>
             <Popup>
               <span>{city.emoji}</span>
               <span>{city.cityName}</span>
             </Popup>
           </Marker>
         ))}
+        <SetForm />
         <ChengeCenter position={position} />
       </MapContainer>
     </div>
@@ -50,7 +55,15 @@ function Map() {
 }
 function ChengeCenter({ position }) {
   const map = useMap();
-  map.setView(position, 10);
+  map.setView(position);
   return null;
 }
+
+function SetForm() {
+  const navigate = useNavigate();
+  const map = useMapEvents({
+    click: (e) => navigate(`form?lat=${e.latlng.lat}&lng=${e.latlng.lng}`),
+  });
+}
+
 export default Map;
